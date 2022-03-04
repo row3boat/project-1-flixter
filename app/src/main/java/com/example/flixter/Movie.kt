ba7c4ef9389ae9e7c8d1aa872f2f882c1dbd4895
row3boat <row3boat@gmail.com>
2022-03-04 00:18:10 -0800

    Added functionality for basic user stories.  Also added addt'l story to only play popular movies.

--- app/src/main/java/com/example/flixter/Movie.kt
+++ app/src/main/java/com/example/flixter/Movie.kt
@@ -1,14 +1,19 @@
 package com.example.flixter
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie(
     val title: String,
     val movieID: Int,
+    val voteAverage : Double,
     val overview: String,
     private val posterPath: String,
-){
-
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
     companion object {
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
@@ -19,6 +24,7 @@ data class Movie(
                     Movie(
                         movieJson.getString("title"),
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("overview"),
                         movieJson.getString("poster_path"),
                     )
